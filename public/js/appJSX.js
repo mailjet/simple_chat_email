6e757f3f8f11f92d9e63f0b7ce5c5e8e13eb15cf
SHARMA Shubham <shubham@sharma.fr>
2015-08-26 23:19:44 +0200

    REact ok

--- public/js/appJSX.js
+++ public/js/appJSX.js
@@ -1,97 +1,155 @@
 /** @jsx React.DOM */
 var MessageList = React.createClass({
-  render: function() {
-    var _this = this;
-    var createItem = function(item, index) {
-      return (
-        <li key={ index }>
-          { item.text }
-        </li>
-      );
-    };
-    return <ul>{ this.props.items.map(createItem) }</ul>;
-  }
+	render: function() {
+		var _this = this;
+		var createItem = function(item, index) {
+			return (
+				<li key={ index }>
+				<strong className='example-chat-username'> {item.name} </strong>
+				<strong className='example-chat-email'> {item.email} </strong>
+				{ item.text }
+				</li>
+			);
+		};
+		return (
+			<ul id='example-messages' className="example-chat-messages">
+				{ this.props.items.map(createItem) }
+			</ul>
+		);
+	}
 });
 
 /** @jsx React.DOM */
 var InfoInput = React.createClass({
-  render: function() {
+	render: function() {
 
-    return (
-    <div>
-      <div className='example-chat-toolbar'>
-        <label for="nameInput">User :</label>
-        <input type='text' id='nameInput' placeholder='enter your name here...'/>
-      </div>
-
-
-      <div className='example-chat-toolbar'>
-        <label for="emailInput">Email :</label>
-        <input type='text' id='emailInput' placeholder='enter an email adresse...'/>
-      </div>
-     </div>
-   	);
-  }
+		return (
+			<div>
+				
+			</div>
+			);
+	}
 });
 
 var MessageSystem = React.createClass({
-  getInitialState: function() {
-    return {
-      items: [],
-      text: ''
-    };
-  },
+	getInitialState: function() {
+		return {
+			items: [],
+			text: '',
+			name: '',
+		    email: ''
+		};
+	},
 
-  componentWillMount: function() {
-    this.firebaseRef = new Firebase('https://chat-email.firebaseio.com/');
-    this.firebaseRef.limitToLast(25).on('value', function(dataSnapshot) {
-      var items = [];
-      dataSnapshot.forEach(function(childSnapshot) {
-        var item = childSnapshot.val();
-        item['.key'] = childSnapshot.key();
-        items.push(item);
-      }.bind(this));
+	componentWillMount: function() {
+		this.firebaseRef = new Firebase('https://chat-email.firebaseio.com/');
+		this.firebaseRef.limitToLast(25).on('value', function(dataSnapshot) {
+			var items = [];
+			dataSnapshot.forEach(function(childSnapshot) {
+				var item = childSnapshot.val();
+				item['.key'] = childSnapshot.key();
+				items.push(item);
+			}.bind(this));
 
-      this.setState({
-        items: items
-      });
-    }.bind(this));
-  },
+			this.setState({
+				items: items
+			});
+		}.bind(this));
+	},
 
-  componentWillUnmount: function() {
-    this.firebaseRef.off();
-  },
+	componentWillUnmount: function() {
+		this.firebaseRef.off();
+	},
 
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
+	onChangeText: function(e) {
+		this.setState({
+			text: e.target.value
+		});
+	},
 
-  handleSubmit: function(e) {
-    e.preventDefault();
-    if (this.state.text && this.state.text.trim().length !== 0) {
-      this.firebaseRef.push({
-        text: this.state.text
-      });
-      this.setState({
-        text: ''
-      });
-    }
-  },
+	onChangeEmail: function(e) {
+		this.setState({
+			email: e.target.value
+		});
+	},
 
-  render: function() {
-    return (
-      <div className="example-chat l-demo-container">
-        <header>Email Based chat</header>
-        <InfoInput/>
-        <MessageList items={ this.state.items } removeItem={ this.removeItem } />
-        <form onSubmit={ this.handleSubmit }>
-          <input onChange={ this.onChange } value={ this.state.text } />
-          <button>{ 'Add #' }</button>
-        </form>
-      </div>
+	onChangeName: function(e) {
+		this.setState({
+			name: e.target.value
+		});
+	},
 
-    );
-  }
+	handleSubmit: function(e) {
+		e.preventDefault();
+		if (this.state.text && this.state.text.trim().length !== 0) {
+		    elt = {
+		      name:this.state.name,
+		      email:this.state.email,
+		      text:this.state.text,
+		      type:"client"
+		    }
+			this.firebaseRef.push(elt);
+			this.setState({
+		      name:'',
+		      email:'',
+		      text:'',
+		      type:''
+			});
+		}
+	},
+
+	render: function() {
+		return (
+			<div className="example-chat l-demo-container">
+
+				<form onSubmit={ this.handleSubmit }>
+					<header>Email Based chat</header>
+					<div className='example-chat-toolbar'>
+						<label htmlFor="nameInput">User :</label>
+						<input 
+							type='text' 
+							id='nameInput'
+							placeholder='enter your name here...' 
+							onChange={ this.onChangeName } 
+							value={ this.state.name } 
+						
+						/>
+					</div>
+
+
+					<div className='example-chat-toolbar'>
+						<label htmlFor="emailInput">Email :</label>
+						<input 
+							type='text' 
+							id='emailInput' 
+							placeholder='enter an email adresse...' 
+							onChange={ this.onChangeEmail } 
+							value={ this.state.email } 
+							
+						/>
+					</div>	
+
+
+					<MessageList items={ this.state.items }/>
+
+
+					
+
+					<footer>
+						<input 
+							type='text' 
+							id='messageInput'  
+							placeholder='Type a message...' 
+							onChange={ this.onChangeText } 
+							value={ this.state.text } 
+						/>
+					</footer>
+					<button>I am not going to be displayed</button>
+				</form>
+			</div>
+
+			);
+	}
 });
 
 React.render(<MessageSystem />, document.getElementById('content'));
