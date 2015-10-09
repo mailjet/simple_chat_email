debd59a2cc93ae6dc6537e7c9af137517314df94
SHARMA Shubham <shubham@sharma.fr>
2015-10-09 10:44:28 +0200

    no more dialog button

--- public/js/appJSX.js
+++ public/js/appJSX.js
@@ -141,40 +141,4 @@ var MessageSystem = React.createClass({
 	}
 });
 
-var ButtonWithDialog = React.createClass({
-    render: function() {
-        return <button onClick={this.handleClick}>
-            Click Me!
-        </button>;
-    },
-    renderLayer: function() {
-        if (this.state.clicked) {
-            return <Modal onClose={this.handleClose}>
-                <div className="modal-header">
-                    Header
-                    <a href="javascript: void 0;"
-                       style={{float: "right", textDecoration: "none"}}
-                       onClick={this.handleClose}>
-                        &#215;
-                    </a>
-                </div>
-                <div className="modal-body">Body!</div>
-            </Modal>;
-        } else {
-            return <div />;
-        }
-    },
-    // {{{
-    handleClose: function() {
-        this.setState({ clicked: false });
-    },
-	handleClick: function() {
-		this.setState({ clicked: !this.state.clicked });
-	},
-	getInitialState: function() {
-		return { clicked: false };
-	}
-	// }}}
-});
-
 React.render(<MessageSystem />, document.getElementById('content'));
